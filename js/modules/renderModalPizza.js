5c9eb5b3e05016208d094afa2d9fbe932905020e
KuzAnton8335 <ant2783@gmail.com>
2024-04-22 12:17:42 +0300

    satrt modal-cart window

--- js/modules/renderModalPizza.js
+++ js/modules/renderModalPizza.js
@@ -2,98 +2,98 @@ import { cartContorl } from "./cartControl.js";
 import { changefirstUppercase, createLabel, createRadioInput } from "./helpers.js";
 
 export const renderModalPizza = ({ id, images, name, price, toppings }) => {
-  const modalPizzaMain = document.querySelector('.modal-pizza__main');
-  modalPizzaMain.textContent = '';
+	const modalPizzaMain = document.querySelector('.modal-pizza__main');
+	modalPizzaMain.textContent = '';
 
-  // размер цены пиццы для вставки в верстку
-  let size = Object.keys(price)[0];
+	// размер цены пиццы для вставки в верстку
+	let size = Object.keys(price)[0];
 
-  // верстка фото пиццы для вставки в верстку(в webp)
-  const picture = document.createElement('picture');
-  const source = document.createElement('source');
-  source.srcset = images[1];
-  source.type = 'image/webp';
+	// верстка фото пиццы для вставки в верстку(в webp)
+	const picture = document.createElement('picture');
+	const source = document.createElement('source');
+	source.srcset = images[1];
+	source.type = 'image/webp';
 
 
-  // верстка фото пиццы для вставки в верстку(в png)
-  const img = document.createElement('img');
-  img.classList.add('modal-pizza__img');
-  img.src = images[0];
-  img.alt = name.ru;
-  picture.append(source, img);
+	// верстка фото пиццы для вставки в верстку(в png)
+	const img = document.createElement('img');
+	img.classList.add('modal-pizza__img');
+	img.src = images[0];
+	img.alt = name.ru;
+	picture.append(source, img);
 
-  // верстка заголовки пиццы для вставки в верстку
-  const title = document.createElement('h2');
-  title.classList.add('modal-pizza__title');
-  title.textContent = changefirstUppercase(name.ru);
+	// верстка заголовки пиццы для вставки в верстку
+	const title = document.createElement('h2');
+	title.classList.add('modal-pizza__title');
+	title.textContent = changefirstUppercase(name.ru);
 
-  // верстка характеристика  пиццы для вставки в верстку
-  const toppingElement = document.createElement('p');
-  toppingElement.classList.add('modal-pizza__tippings');
-  toppingElement.textContent = changefirstUppercase(toppings.ru);
+	// верстка характеристика  пиццы для вставки в верстку
+	const toppingElement = document.createElement('p');
+	toppingElement.classList.add('modal-pizza__tippings');
+	toppingElement.textContent = changefirstUppercase(toppings.ru);
 
-  const priceSizeInfo = document.createElement('p');
-  priceSizeInfo.classList.add('modal-pizza__info');
+	const priceSizeInfo = document.createElement('p');
+	priceSizeInfo.classList.add('modal-pizza__info');
 
-  const priceElement = document.createElement('span');
-  priceElement.classList.add("modal-pizza__price");
-  const slashElement = document.createElement('span');
-  slashElement.textContent = `/`
-  const sizeElement = document.createElement('span');
-  sizeElement.classList.add("modal-pizza__sm");
+	const priceElement = document.createElement('span');
+	priceElement.classList.add("modal-pizza__price");
+	const slashElement = document.createElement('span');
+	slashElement.textContent = `/`
+	const sizeElement = document.createElement('span');
+	sizeElement.classList.add("modal-pizza__sm");
 
-  //функция вставки цены и размера пиццы
-  const updatePrice = () => {
-    const selectedSizeInput = form.querySelector('input[name="size"]:checked');
-    size = selectedSizeInput.value;
-    priceElement.textContent = `${price[size]} ₽`;
-    sizeElement.textContent = `${parseInt(size)} см`;
-  };
+	//функция вставки цены и размера пиццы через форму/input[name="size"]
+	const updatePrice = () => {
+		const selectedSizeInput = form.querySelector('input[name="size"]:checked');
+		size = selectedSizeInput.value;
+		priceElement.textContent = `${price[size]} ₽`;
+		sizeElement.textContent = `${parseInt(size)} см`;
+	};
 
-  priceSizeInfo.append(priceElement, slashElement, sizeElement);
+	priceSizeInfo.append(priceElement, slashElement, sizeElement);
 
-  // create form - создание формы
-  const form = document.createElement("form");
-  form.id = id;
-  form.classList.add("modal-pizza__form");
+	// create form - создание формы
+	const form = document.createElement("form");
+	form.id = id;
+	form.classList.add("modal-pizza__form");
 
-  const groupFieldset = document.createElement("div");
-  groupFieldset.classList.add("modal-pizza__group-fieldset");
+	const groupFieldset = document.createElement("div");
+	groupFieldset.classList.add("modal-pizza__group-fieldset");
 
-  const fieldsetCrust = document.createElement("fieldset");
-  fieldsetCrust.classList.add("modal-pizza__fieldset");
+	const fieldsetCrust = document.createElement("fieldset");
+	fieldsetCrust.classList.add("modal-pizza__fieldset");
 
-  const thickInput = createRadioInput("thick", "crust", "thick", "modal-pizza__radio");
-  thickInput.checked = true;
-  const thickLabel = createLabel("modal-pizza__label", "thick", "Пышное тесто");
+	const thickInput = createRadioInput("thick", "crust", "thick", "modal-pizza__radio");
+	thickInput.checked = true;
+	const thickLabel = createLabel("modal-pizza__label", "thick", "Пышное тесто");
 
-  const thinInput = createRadioInput("thin", "crust", "thin", "modal-pizza__radio");
-  const thinLabel = createLabel("modal-pizza__label", "thin", "Тонкое тесто");
+	const thinInput = createRadioInput("thin", "crust", "thin", "modal-pizza__radio");
+	const thinLabel = createLabel("modal-pizza__label", "thin", "Тонкое тесто");
 
-  fieldsetCrust.append(thickInput, thickLabel, thinInput, thinLabel)
+	fieldsetCrust.append(thickInput, thickLabel, thinInput, thinLabel)
 
-  const fieldsetSize = document.createElement("fieldset");
-  fieldsetSize.classList.add("modal-pizza__fieldset");
+	const fieldsetSize = document.createElement("fieldset");
+	fieldsetSize.classList.add("modal-pizza__fieldset");
 
-  // render при помощи Object.keys и forEach
-  const sizeInputs = Object.keys(price).map(size => createRadioInput(size, "size", size, "modal-pizza__radio"));
-  sizeInputs[0].checked = true;
-  sizeInputs.forEach(input => {
-    const label = createLabel("modal-pizza__label", input.id, `${input.value} см`);
-    input.addEventListener('change', updatePrice)
-    fieldsetSize.append(input, label);
-  })
+	// render при помощи Object.keys и forEach
+	const sizeInputs = Object.keys(price).map(size => createRadioInput(size, "size", size, "modal-pizza__radio"));
+	sizeInputs[0].checked = true;
+	sizeInputs.forEach(input => {
+		const label = createLabel("modal-pizza__label", input.id, `${input.value} см`);
+		input.addEventListener('change', updatePrice)
+		fieldsetSize.append(input, label);
+	})
 
-  const addToCartBtn = document.createElement("button");
-  addToCartBtn.classList.add("modal-pizza__add-cart");
-  addToCartBtn.textContent = "В корзину";
+	const addToCartBtn = document.createElement("button");
+	addToCartBtn.classList.add("modal-pizza__add-cart");
+	addToCartBtn.textContent = "В корзину";
 
-  groupFieldset.append(fieldsetCrust, fieldsetSize);
+	groupFieldset.append(fieldsetCrust, fieldsetSize);
 
-  form.append(groupFieldset, addToCartBtn);
-  const closeBtn = document.createElement("button");
-  closeBtn.classList.add("modal__close");
-  closeBtn.innerHTML = `
+	form.append(groupFieldset, addToCartBtn);
+	const closeBtn = document.createElement("button");
+	closeBtn.classList.add("modal__close");
+	closeBtn.innerHTML = `
   <svg width="20.000000" height="20.000000" viewBox="0 0 20 20" fill="none"     xmlns="http://www.w3.org/2000/svg"
    xmlns:xlink="http://www.w3.org/1999/xlink">
    <rect id="close" width="20.000000" height="20.000000" fill="#FFFFFF" fill-opacity="0" />
@@ -103,41 +103,41 @@ export const renderModalPizza = ({ id, images, name, price, toppings }) => {
    transform="rotate(-45 4.000000 4.601807)" fill="#C1AB91" fill-opacity="1.000000" />
  </svg>
   `
-  modalPizzaMain.append(picture, title, toppingElement, priceSizeInfo, form, closeBtn);
+	modalPizzaMain.append(picture, title, toppingElement, priceSizeInfo, form, closeBtn);
 
-  updatePrice();
+	updatePrice();
 
-  let timerId = NaN;
-  // функция добавления в корзину товара(пиццы)
-  form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    // формирование хранилища для корзины
-    const formData = new FormData(form);
+	let timerId = NaN;
+	// функция добавления в корзину товара(пиццы)
+	form.addEventListener("submit", (e) => {
+		e.preventDefault();
+		// формирование хранилища для корзины
+		const formData = new FormData(form);
 
-    // формирование товара для карзины из input(html),присвоение для него id и cartId
-    const product = {
-      cartId: crypto.randomUUID(),
-      id,
-      crust: formData.get('crust'),
-      size: formData.get('size'),
-    }
+		// формирование товара для карзины из input(html),присвоение для него id и cartId
+		const product = {
+			cartId: crypto.randomUUID(),
+			id,
+			crust: formData.get('crust'),
+			size: formData.get('size'),
+		}
 
-    // добавление товара в localStorage при помощи функции CartControl
-    cartContorl.addCart(product);
+		// добавление товара в localStorage при помощи функции CartControl
+		cartContorl.addCart(product);
 
-    addToCartBtn.disabled = true;
-    addToCartBtn.textContent = "Добавлено";
-    // время выдержки кнопки в корзине
-    timerId = setTimeout(() => {
-      addToCartBtn.disabled = false;
-      addToCartBtn.textContent = "В козину";
-    }, 3000)
-  });
+		addToCartBtn.disabled = true;
+		addToCartBtn.textContent = "Добавлено";
+		// время выдержки кнопки в корзине
+		timerId = setTimeout(() => {
+			addToCartBtn.disabled = false;
+			addToCartBtn.textContent = "В козину";
+		}, 3000)
+	});
 
-  // функция что бы кнопка была "В корзину"
-  form.addEventListener("change", () => {
-    clearTimeout(timerId);
-    addToCartBtn.disabled = false;
-    addToCartBtn.textContent = "В козину";
-  });
+	// функция что бы кнопка была "В корзину"
+	form.addEventListener("change", () => {
+		clearTimeout(timerId);
+		addToCartBtn.disabled = false;
+		addToCartBtn.textContent = "В козину";
+	});
 }
--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
+import { modalController } from "./modules/modalController.js";
+import { renderCart } from "./modules/renderCart.js";
 import { renderPizza } from "./modules/renderPizza.js";
 import { renderToppings } from "./modules/renderToppings.js";
 import { toppingToggle } from "./modules/toppingToggle.js";
 
 
 const init = () => {
-	toppingToggle();
-	renderToppings();
-	renderPizza();
+  toppingToggle();
+  renderToppings();
+  renderPizza();
+  modalController({
+    modal: ".modal-cart",
+    btnOpen: ".header__cart",
+    btnClose: ".modal__close",
+    cbOpen() {
+      renderCart();
+    }
+  });
 }
 
 init();
